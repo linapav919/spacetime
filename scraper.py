fbf250d142b8a60d8b93d7f86c9b3b3c2bbbe636
Lina Pav <lpav@uci.edu>
2023-11-10 19:26:22 -0800

    add code to scraper.py

--- scraper.py
+++ scraper.py
@@ -5,36 +5,60 @@ def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
+
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    # Check if the response status is valid (e.g., 200 OK)
+    if resp.status == 200:
+        # Initialize an empty list to store URLs
+        urls_to_crawl = [
+    "http://ics.uci.edu/",
+    "http://cs.uci.edu/",
+    "http://informatics.uci.edu/",
+    "http://stat.uci.edu/"]
+
+
+      # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+        # Find and extract links from the page
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href:
+                # Check if the URL meets the criteria for further crawling
+                if is_valid(href):
+                    urls_to_crawl.append(href)
+
+        return urls_to_crawl
+
+    # Return an empty list for non-200 responses
+    return []
 
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
-    # If you decide to crawl it, return True; otherwise return False.
-    # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
+        # Check if the URL scheme is either 'http' or 'https'
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
-    except TypeError:
-        print ("TypeError for ", parsed)
-        raise
+        # Define a list of file extensions to filter out
+        invalid_extensions = [
+            "css", "js", "bmp", "gif", "jpeg", "jpg", "ico",
+            "png", "tiff", "mid", "mp2", "mp3", "mp4",
+            "wav", "avi", "mov", "mpeg", "ram", "m4v", "mkv", "ogg", "ogv", "pdf",
+            "ps", "eps", "tex", "ppt", "pptx", "doc", "docx", "xls", "xlsx", "names",
+            "data", "dat", "exe", "bz2", "tar", "msi", "bin", "7z", "psd", "dmg", "iso",
+            "epub", "dll", "cnf", "tgz", "sha1", "thmx", "mso", "arff", "rtf", "jar", "csv",
+            "rm", "smil", "wmv", "swf", "wma", "zip", "rar", "gz"]
+
+            # Check if the URL path ends with any of the invalid extensions
+        if any(url.endswith("." + ext) for ext in invalid_extensions):
+                return False
+
+            # Add custom filtering rules here, if needed
+
+            # If the URL passes all checks, it is considered valid
+        return True
+
+    except Exception:
+        # Handle exceptions gracefully, e.g., for invalid URLs
+        return False
